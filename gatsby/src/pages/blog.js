e8869489d081ffdc64b63b943f587ba52ce00a94
Petr Bukov <pbu.vsl@icloud.com>
2022-03-27 13:02:30 +0300

    feat: create Layout component

--- gatsby/src/pages/blog.js
+++ gatsby/src/pages/blog.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Nav from '../components/Nav';
 
 export default function BlogPage() {
   return (
-    <div>
-      <Nav />
+    <>
       <p>Blog</p>
-    </div>
+    </>
   );
 }
--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import Nav from '../components/Nav';
 
 // latest essay: 1 items
 // crucial essays: 4 items
 export default function HomePage() {
   return (
-    <div>
-      <Nav />
+    <>
       <p>Hot Essays</p>
-    </div>
+      <p>Hey</p>
+    </>
   );
 }
